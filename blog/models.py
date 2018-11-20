cf21045e5a5c01924ae89eef1546faee7d6e5645
Dosimz <vhzix24@gmail.com>
2018-11-20 23:12:34 +0800

    美化了Blog页面
    Blog主页只显示少量的正文

--- blog/models.py
+++ blog/models.py
@@ -9,5 +9,10 @@ class Blog (models.Model):
     image = models.ImageField(default='default.jpg',upload_to='images/')
     text = models.TextField(default='正文题')
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
+
+    def summary_text(self):
+        sum = str(self.text)
+        return sum[:20] + '...'
+            
