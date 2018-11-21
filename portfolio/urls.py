1c2565ee2f2a584fd7cc6df3231d04aa4ba5b8bc
Dosimz <vhzix24@gmail.com>
2018-11-21 20:49:46 +0800

    实现了从blog 标题到正文的跳转；还添加了pdf文件和照片
    静态文件

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-import blog.views
+# import blog.views
 from django.conf.urls.static import static
 from django.conf import settings
 
